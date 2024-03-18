0d253e7faa21ed2a7f5231e92670fd37b93cee6f
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-03-18 21:26:00 +0100

    Add application baseline
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

---
+++ pkg/application/application.go
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2024 CoreLayer BV
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package application
+
+import "github.com/spf13/cobra"
+
+func NewApplication(c *cobra.Command, v Version) *Application {
+	if v.runE != nil {
+		c.AddCommand(v.Command())
+	}
+
+	if c.Version != "" {
+		c.Version = ""
+	}
+
+	return &Application{
+		Root:    c,
+		Version: v,
+	}
+}
+
+type Application struct {
+	Root    *cobra.Command
+	Version Version
+}
+
+func (a *Application) RegisterCommands(c []Commander, f func(cmd *cobra.Command)) {
+	for _, cmdr := range c {
+		a.Root.AddCommand(cmdr.Initialize(f))
+	}
+}
+
+func (a *Application) Run() error {
+	return a.Root.Execute()
+}
